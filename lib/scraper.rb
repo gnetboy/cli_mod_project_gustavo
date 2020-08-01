301f087dc31234d2714852a372ee08bde81c9b00
Gustavo <gnetboy.y@gmail.com>
2020-08-01 12:51:53 -0700

    scraper methods defined

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,35 +5,31 @@ require 'pry'
 class Scraper
    
   def scrape_city_urls
-    california_url="http://www.museumsusa.org/museums/?k=1271400%2cState%3aCA%3bDirectoryID%3a200454"
+      california_url="http://www.museumsusa.org/museums/?k=1271400%2cState%3aCA%3bDirectoryID%3a200454"
       html=open(california_url)
       doc=Nokogiri::HTML(html)
-      
-      cities=doc.css('#categoryid').css('.browseCategoryItem')
-      binding.pry
-
+      cities=doc.css('#categoryid').css('.browseCategoryItem').css('a')
       city_urls=[]
 
       cities.each do |city|
         url =city.attribute('href').value
-       city_urls << url
-       
+        city_urls << url
       end
       scrape_city_pages(city_urls)
-   end
+  end
 
-#    def scrape_city_pages(city_urls)
-#         museums_list=[]
-       
-#         city_urls.each do |city_url|
-#           url="http://www.museumsusa.org/#{city_url}"
-#           html=open(url)
-#           doc=Nokogiri::HTML(html)
+   def scrape_city_pages(city_urls)
+        
+        museums_list=[]
+        city_urls.each do |city_url|
+          url="http://www.museumsusa.org#{city_url}"
+          html=open(url)
+          doc=Nokogiri::HTML(html)
           
-#         museums_list << doc.css('div.basic:nth-child(2)').css('div.basic:nth-child(2) > div:nth-child(1)')
-    
-#       end
-#     end
+        museums_list << doc.css('div.itemGroup').css('.item').css('.basic')
+        end
+        binding.pry
+    end
 
     # def create_museums(museums_list)
     #     museums_list.each do |museum|
