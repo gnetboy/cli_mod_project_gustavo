7e4221e5764980ac48da1335c4f5dc8f083a064f
Gustavo <gnetboy.y@gmail.com>
2020-08-01 10:40:38 -0700

    added scraper

---
+++ README.md
@@ -0,0 +1,13 @@
+#Museums in the state of California
+
+A CLI application that shows you the user to see information of all the museums in the state of California. To accomplish this task the app will attempt to get all this information from Museumsusa.org. Which conveniently presents information of museums by state great!.
+
+This app was built in Ruby 2.6.1
+to run the app you will need a local installation of Ruby.
+
+Fork the  repo and clone to your local machine.
+to star the app at a terminal prompt:
+
+$ bin/run
+
+Once you start the  app, a list of all the museums in California will be presented to you.
---
+++ lib/scraper.rb
@@ -0,0 +1,50 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Scraper
+   
+  def scrape_city_urls
+    california_url="http://www.museumsusa.org/museums/?k=1271400%2cState%3aCA%3bDirectoryID%3a200454"
+      html=open(california_url)
+      doc=Nokogiri::HTML(html)
+      
+      cities=doc.css('#categoryid').css('.browseCategoryItem')
+      binding.pry
+
+      city_urls=[]
+
+      cities.each do |city|
+        url =city.attribute('href').value
+       city_urls << url
+       
+      end
+      scrape_city_pages(city_urls)
+   end
+
+#    def scrape_city_pages(city_urls)
+#         museums_list=[]
+       
+#         city_urls.each do |city_url|
+#           url="http://www.museumsusa.org/#{city_url}"
+#           html=open(url)
+#           doc=Nokogiri::HTML(html)
+          
+#         museums_list << doc.css('div.basic:nth-child(2)').css('div.basic:nth-child(2) > div:nth-child(1)')
+    
+#       end
+#     end
+
+    # def create_museums(museums_list)
+    #     museums_list.each do |museum|
+    #      museums_list.css('div.basic:nth-child(2) > div:nth-child(1)').css('#ctl12_ctl00_rptContacts_org_0 > a:nth-child(1)').text
+    #      binding.pry
+          
+    #     end
+    # end
+
+   
+end
+
+scrape=Scraper.new
+scrape.scrape_city_urls
