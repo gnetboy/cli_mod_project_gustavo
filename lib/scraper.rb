9d7f6bbfea28ca9df2b4b00e05d448b2753eff48
Gustavo <gnetboy.y@gmail.com>
2020-08-02 22:58:55 -0700

    added more classes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,41 +3,55 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+
+  MAIN_PATH="http://www.museumsusa.org/museums/?k=1271404%2cCity%3aSan+Francisco%3bState%3aCA%3bDirectoryID%3a200454"
    
   def scrape_city_urls
-      san_francisco_url="http://www.museumsusa.org/museums/?k=1271404%2cCity%3aSan+Francisco%3bState%3aCA%3bDirectoryID%3a200454"
-      html=open(san_francisco_url)
-      doc=Nokogiri::HTML(html)
-      cities=doc.css('.browseCategory').css('.browseCategoryItem').css('a')
+       #This method returns the url of museums in our targe city in this case San Francisco
+      doc=Nokogiri::HTML(open(MAIN_PATH))
       city_urls=[]
+      cities=doc.css('.browseCategory').css('.browseCategoryItem').css('a')
 
       cities.each do |city|
         url =city.attribute('href').value
         city_urls << url
       end
       scrape_city_pages(city_urls)
-  end
-
+    end
+    
     def scrape_city_pages(city_urls)
-        
+      #this method returns an array of hashes with info of all the museums
       museums_list=[]
       city_urls.each do |city_url|
-      url="http://www.museumsusa.org#{city_url}"
-      html=open(url)
-      doc=Nokogiri::HTML(html)
-      
-      museums_list << doc.css('.itemGroup').css('div.basic:nth-child(2)')
+        doc=Nokogiri::HTML(open("http://www.museumsusa.org#{city_url}"))
+        museums_list << doc.css('.itemGroup').css('div.basic:nth-child(2)')
       end
      create_museums(museums_list)
-    end                
-
+    end    
+    
+    
+    
     def create_museums(museums_list)
-        museums_list.each do |museum|
-          name=museum.css('div.basic:nth-child(2) > div:nth-child(1)').css('a:nth-child(1)').text
-          binding.pry
-       end
+      
+      museums=[]
+      museums_list.each do |museum|
+        name = museum.css('div.basic:nth-child(2) > div:nth-child(1)').css('a:nth-child(1)').text
+        location = museum.css('.location').text.split(', ')
+        type= museum.css('.type').text
+        description = museum.css('.abstract').text
+        
+        museum_info = {
+          :name => name,
+          :city => location[0],
+          :state => location[1],
+          :description => description,
+          :type => type
+        }
+        museums << museum_info  
+      end
+      museums
+      binding.pry 
     end
-
    
 end
 #doc.css('.itemGroup').css('div.basic:nth-child(2)').css('div.basic:nth-child(2) > div:nth-child(1)').css('a:nth-child(1)').text
